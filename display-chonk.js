fc09b93eb4c07809739bcbd46aa19e9c46a0761a
sethripman <sethripman@gmail.com>
2019-10-08 10:06:02 -0700

    finished data, working on display function

--- display-chonk.js
+++ display-chonk.js
@@ -1,16 +1,15 @@
 function displayChonk(chonk) {
-    const li = document.createElement('li');
-    li.className = chonk.category;
-    li.title = chonk.description;
+    const newChonk = document.createElement('li');
+    newChonk.title = chonk.description;
 
     const h3 = document.createElement('h3');
     h3.textContent = chonk.name;
-    li.appendChild(h3);
+    newChonk.appendChild(h3);
 
     const img = document.createElement('img');
     img.src = '../assets/' + chonk.image;
     img.alt = chonk.name + ' image';
-    li.appendChild(img);
+    newChonk.appendChild(img);
 
     const p = document.createElement('p');
     p.className = 'price';
@@ -21,15 +20,19 @@ function displayChonk(chonk) {
     //     currency: 'USD' 
     // });
     p.textContent = usd;
+
+    const p2 = document.createElement('p');
+    p2.className = 'size';
+    newChonk.appendChild(p2);
     
     const button = document.createElement('button');
     button.textContent = 'Add';
     button.value = chonk.code;
     p.appendChild(button);
 
-    li.appendChild(p);
+    newChonk.appendChild(p);
 
-    return li;
+    return newChonk;
 }
 
 export default displayChonk;
--- product/chonks.js
+++ product/chonks.js
@@ -2,21 +2,43 @@ const gday = {
     id: 'gday',
     name: 'Gday Mate',
     image: 'gday.png',
-    description: 'The King from Down Under whose movement rolls like thunder',
-    category: 'chonk-img',
-    price: 1.00,
-    cost: 0.25
+    description: 'The King from Down Under whose movements roll like thunder',
+    price: 6.00,
+    size: 'Wonderfully Hefty'
 };
 
+const mrbigs = {
+    id: 'mrbigs',
+    name: 'Monsieur Bigs',
+    image: 'mrbigs.png',
+    description: 'A sizeable feline with chonkiness divine',
+    price: 3.50,
+    size: 'Professionally Agglomerated but he never sated'
+};
+
+const baby = {
+    id: 'baby',
+    name: 'Ickle Mister',
+    image: 'baby.png',
+    description: 'So sleepy! So cheap! Destined to *become* the top of the heap',
+    price: 2.00,
+    size: 'Petite yet waddling'
+};
+
+const boss = {
+    id: 'boss',
+    name: 'Emperor Bossman',
+    image: 'boss.png',
+    description: 'Often late for a meeting cause Him busy feeding',
+    price: 9.00,
+    size: 'Simply Gargantuan'
+};
 
 const chonks = [
     gday,
-    banana,
-    blueberry,
-    cherry,
-    greenApple,
-    mango,
-    orange
+    mrbigs,
+    baby,
+    boss
 ];
 
 export default chonks;
