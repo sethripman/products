de2d7e3b8ac7108a4bd164de73fb949a8967f19c
sethripman <sethripman@gmail.com>
2019-10-09 11:10:47 -0700

    added order total cell display to app js

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,7 +1,7 @@
 import renderTableRow from './render-table-row.js';
 import cart from '../data/cart.js';
 import chonks from '../data/chonks.js';
-import { findByID } from '../common/utils.js';
+import { findByID, calcOrderTotal } from '../common/utils.js';
 
 const tableElement = document.querySelector('tbody');
 
@@ -13,28 +13,5 @@ cart.forEach(chonkOrder => {
 
 });
 
-/*
-
-Alternate loop for each method, more readable
-
-let cartTotal;
-cart.forEach(chonkOrder => {
-    chonks.forEach(chonk => {
-        let chonkTotal = 0;
-
-        if (chonk.id === chonkIDFromOrder) {
-            const row = renderTableRow(chonk, chonkOrder);
-
-            tableElement.appendChild(row);
-
-            chonkTotal = chonk.price * chonkOrder.quantity;
-            cartTotal += chonkTotal;
-    }
-}
-}) 
-
-
 const totalCell = document.getElementById('order-total-cell');
-totalCell.textContent = cartTotal;
-
-*/
+totalCell.textContent = calcOrderTotal(cart, chonks);
